23b4d03e0fa5498700fd0d41d5a5a5eac718f989
Zack Carlson <jzcarlson014@gmail.com>
2020-08-26 18:54:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |str|
-    str 
+    str if planeteer_calls.include?(valid_calls.each)
+  end
 end
